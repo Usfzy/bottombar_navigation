6645e4d82cdfab50beee39cec2ebbea3ee44cb24
Usfzy <usfzy47@gmail.com>
2023-11-12 19:46:43 +0000

    initial commit

---
+++ app/src/main/java/com/example/bottombarandroid/fragments/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.example.bottombarandroid
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/bottombarandroid/fragments/TicketsFragment.kt
@@ -0,0 +1,22 @@
+package com.example.bottombarandroid.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.bottombarandroid.R
+
+class TicketsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        return view;
+    }
+}
